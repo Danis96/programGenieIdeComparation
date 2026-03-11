d2fc611f19f30b77a015f9d1b1fc35191fc56f81
DANIS <>
2026-03-11 17:06:21 +0100

    space logic

--- lib/app/helpers/differences_list_helper.dart
+++ lib/app/helpers/differences_list_helper.dart
@@ -58,6 +58,8 @@ class DifferencesListHelper {
       return version2Differences[section] ??
           'No Version 2 data yet for this section.';
     }
+
+
     return prewrittenDifferences[section] ?? '';
   }
 }
